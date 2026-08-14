556f71f88631b81fa8d57de1ffd61990fe14ee1f
ishak595-code <ishak595@gmail.com>
2026-08-14 11:46:06 +0200

    V37: mount PWA install experience globally

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,19 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/theme.service';
 import { SeoService } from './services/seo.service';
+import { PwaInstallComponent } from './components/pwa-install.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, PwaInstallComponent],
+  template: `<router-outlet></router-outlet><app-pwa-install></app-pwa-install>`
 })
 export class AppComponent implements OnInit {
   themeService = inject(ThemeService);
   seoService = inject(SeoService);
 
   ngOnInit() {
-    // Initialize SEO and tracking scripts
     this.seoService.init();
   }
 }

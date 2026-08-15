38c1ff5b744635266d2d4957ff01e06b205e8870
ishak595-code <ishak595@gmail.com>
2026-08-15 16:09:19 +0200

    Remove misunderstood global mobile command dock

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,16 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/theme.service';
 import { SeoService } from './services/seo.service';
-import { CustomerMobileDockComponent } from './components/customer-mobile-dock.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CustomerMobileDockComponent],
-  template: `
-    <router-outlet></router-outlet>
-    <app-customer-mobile-dock></app-customer-mobile-dock>
-  `
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
   themeService = inject(ThemeService);

ea8bda2b41a4a84a9b356716c0337c2bf03ce0fd
ishak595-code <ishak595@gmail.com>
2026-08-15 15:48:20 +0200

    Mount customer mobile dock globally outside admin

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,19 +2,22 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/theme.service';
 import { SeoService } from './services/seo.service';
+import { CustomerMobileDockComponent } from './components/customer-mobile-dock.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, CustomerMobileDockComponent],
+  template: `
+    <router-outlet></router-outlet>
+    <app-customer-mobile-dock></app-customer-mobile-dock>
+  `
 })
 export class AppComponent implements OnInit {
   themeService = inject(ThemeService);
   seoService = inject(SeoService);
 
   ngOnInit() {
-    // Initialize SEO and tracking scripts
     this.seoService.init();
   }
 }

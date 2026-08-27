1eab20df1fd1b76d6c7e8f0916189de145b67ab3
ishak595-code <ishak595@gmail.com>
2026-08-27 19:49:05 +0200

    chore(pwa): rotate V197 detail cache generation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const RELEASE = 'v194-home-runtime-jank';
+const RELEASE = 'v197-detail-integrity';
 const CACHE_PREFIX = 'alperler-pwa-';
 const SHELL_CACHE = `${CACHE_PREFIX}${RELEASE}-shell`;
 const STATIC_CACHE = `${CACHE_PREFIX}${RELEASE}-static`;
